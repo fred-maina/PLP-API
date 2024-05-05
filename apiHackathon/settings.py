8f5a33f45e22151745028cff6fa9c09a941e4f1f
fred-maina <fchege04@gmail.com>
2024-05-05 23:02:47 +0300

    Fix Static files not loaded issue

--- apiHackathon/settings.py
+++ apiHackathon/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "quizzy/staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
