5da8a13425f96f4ec5b049f70373b8d2be03bbb0
fred-maina <fchege04@gmail.com>
2024-05-05 23:09:16 +0300

    Update settings.py

--- apiHackathon/settings.py
+++ apiHackathon/settings.py
@@ -116,9 +116,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "quizzy/staticfiles",
-]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
