bce285b017e9ee2356546d0a8fa8ec91e81ba8aa
fred-maina <fchege04@gmail.com>
2024-06-17 18:49:32 +0300

    Add better static file handling

--- apiHackathon/settings.py
+++ apiHackathon/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "quizzy/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
